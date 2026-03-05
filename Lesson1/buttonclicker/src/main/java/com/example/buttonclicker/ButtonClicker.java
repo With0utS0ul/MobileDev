118217e9a7bce4595fc66a47fe834b2cd2e30026
With0utS0ul <ananakarova@gmail.com>
2026-03-06 00:40:28 +0300

    Lesson1

---
+++ Lesson1/buttonclicker/src/main/java/com/example/buttonclicker/ButtonClicker.java
@@ -0,0 +1,52 @@
+package com.example.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ButtonClicker extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBoxX;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_button_clicker);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textViewStudent = findViewById(R.id.textViewStudent);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBoxX = findViewById(R.id.checkBoxX);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № Х");
+                checkBoxX.setChecked(false);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
+
+    }
+
+    public void oclBtnItIsNotMe(View view) {
+        textViewStudent.setText("Это был/а не я!");
+        checkBoxX.setChecked(true);
+    }
+}
